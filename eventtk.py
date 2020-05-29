6a2d46e087cd6a1fb1aff2b923b52810cc66f5ab
Sersovi <sersovi@pm.me>
2020-05-29 17:17:18 +0200

    Actual status for github

--- eventtk.py
+++ eventtk.py
@@ -3,59 +3,48 @@
 An object reacting to an action
 """
 
-def main():
-    running()
 
-def running():
+import tkinter as tk
+import time
 
-    import tkinter as tk
-    import time
-    
-    
-    
-    frame = tk.Tk()
-    canvas = tk.Canvas(frame, width=400,height=400)
-    btn_start = tk.Button(frame, text = "Start Game!", command=moveright)
-    
-    canvas.pack()
-    btn_start.pack()
-    
-    canvas.create_polygon(100,100,100,110,110,110,110,100)
-    
-    def printest():
-        print("command works")
-    
-    def moveup(event):
-        while True:
-            canvas.move(1,0,-1)
-            canvas.update()
-            time.sleep(0.02)
-     
-    def moveright(event):
-        while True:
-            canvas.move(1,1,0)
-            canvas.update()
-            time.sleep(0.02)
-            
-    def moveleft(event):
-        while True:
-            canvas.move(1,-1,0)
-            canvas.update()
-            time.sleep(0.02)
-            
-    def movedown(event):
-        while True:
-            canvas.move(1,0,1)
-            canvas.update()
-            time.sleep(0.02)
-            
-    canvas.bind('<KeyPress-Up>', moveup) # <- It BINDS(event, reaction)
-    canvas.bind('<KeyPress-Down>', movedown)
-    canvas.bind('<KeyPress-Right>', moveright)
-    canvas.bind('<KeyPress-Left>', moveleft)
-    
-    
-    frame.mainloop()
+def moveup(event=None): ### arg = None hace que se pueda dejar vacio, o que se pueda llenar
+    while True: ### <--- loop infinito que no acaba aunque salgas de la fnc.
+        canvas.move(poly1,0,-1)
+        canvas.update()
+        time.sleep(0.02)
+ 
+def moveright(event=None):
+    while True:
+        canvas.move(poly1,1,0)
+        canvas.update()
+        time.sleep(0.02)
+        
+def moveleft(event=None):
+    while True:
+        canvas.move(poly1,-1,0)
+        canvas.update()
+        time.sleep(0.02)
+        
+def movedown(event=None):
+    while True:
+        canvas.move(poly1,0,1)
+        canvas.update()
+        time.sleep(0.02)
 
-if __name__ == '__main__':
-    main()
+frame = tk.Tk()
+canvas = tk.Canvas(frame, width=400,height=400)
+# btn_start = tk.Button(frame, text = "Start Game!", command = moveup) ### moveup --el argumento-- != moveup() --el return de la fnc--
+
+canvas.pack()
+
+
+poly1 = canvas.create_rectangle(200,200,210,210)
+
+        
+canvas.bind('<KeyPress>w', moveup) # <- It BINDS(event, reaction)
+canvas.bind('<KeyPress>s', movedown)
+canvas.bind('<KeyPress>d', moveright)
+canvas.bind('<KeyPress>a', moveleft)
+
+
+frame.mainloop()
